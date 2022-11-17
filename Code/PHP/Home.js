1da4c13472bad5f39459f82bfb85f0c335689d75
Oscar <ramirez.fernandez.oscar@alumnat.copernic.cat>
2022-11-17 02:11:33 +0100

    Creación Home

---
+++ Code/PHP/Home.js
@@ -0,0 +1,53 @@
+class Actividad {
+    constructor(name, description, divisa) {
+        this.name = name;
+        this.description = description;
+        this.divisa = divisa;
+
+    }
+}
+
+class UI {
+    addAct(product) {
+        const actList = document.getElementById('table-act');
+        const element = document.createElement('tr');
+        element.innerHTML = `
+                            <td id=td-act>${product.name}</td>
+                            <td id=td-act>${product.description}</td>
+                            <td id=td-act>${product.divisa}</td>
+                        
+                        <button name="btn-delete" class="btn-delete">DELETE</button>
+        `;
+        actList.appendChild(element);
+
+    }
+    resetForm() {
+        document.getElementById('act-form').reset();
+    }
+    deleteAct(element) {
+        if (element.name === 'btn-delete') {
+            element.parentElement.remove();
+        }
+    }
+
+
+}
+
+document.getElementById('act-form')
+    .addEventListener('submit', function (e) {
+        const name = document.getElementById('name').value;
+        const description = document.getElementById('description').value;
+        const divisa = document.getElementById('divisa').value;
+
+
+        const act = new Actividad(name, description, divisa);
+        const ui = new UI();
+        ui.addAct(act);
+        ui.resetForm();
+
+        e.preventDefault();
+    });
+document.getElementById('act-list').addEventListener('click', function (e) {
+    const ui = new UI();
+    ui.deleteAct(e.target);
+});
--- Code/PHP/Home.php
+++ Code/PHP/Home.php
@@ -10,22 +10,54 @@ include 'nav.php'
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bienvenida</title>
+    <link rel="stylesheet" href="Home.css">
+    <title>Home</title>
 </head>
+
 <body>
+    <h1><?php echo "<h1>Bienvenido - " . $_SESSION['usuario'] . "</h1>"; ?></h1>
 
+    <div class=act-card>
+        <div class="card">
+            <div class="card-body">
+                <h4>AÑADE UNA ACTIVIDAD</h4>
+                <form id="act-form">
+                    <div class="form-group">
+                        <input type="text" id="name" placeholder="Nombre de la actividad" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" id="description" placeholder="Descripción de la actividad" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <select name="divisa" id='divisa' class="form-control">
+                            <option value="$">$</option>
+                            <option value="€">€</option>
+                            <option value="¥">¥</option>
+                        </select>
+                    </div>
+                    <input type="submit" value="ENVIAR" class="btn-card">
+                </form>
+            </div>
+        </div>
+        <div id="act-list">
+            <table id="table-act">
+                <tr>
+                    <td>NOM</td>
+                    <td>DESCRIPCIÓN</td>
+                    <td>DIVISA</td>
+                </tr>
 
-   <h1><?php echo "<h1>Bienvenido - " .$_SESSION['usuario']."</h1>";?></h1>
-    <a href="login/cerrar_sesion.php">Cerrar sesión</a>
-
-    <div class="Card">
-
-    
+            </table>
+        </div>
     </div>
+
+
+    <script src="Home.js"></script>
 </body>
 
 </html>
