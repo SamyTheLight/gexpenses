170cf28efa3ee3d13cc36669e64d037499f7451d
SamyTheLight <samythelight@gmail.com>
2022-11-27 13:08:08 +0100

    header y footer rediseñados

--- Code/PHP/footer.php
+++ Code/PHP/footer.php
@@ -8,7 +8,23 @@
     <title>Document</title>
 </head>
 <body>
-    
-    <footer> <p> Membres: Oscar Ramirez, Joan Canals, Samuel Garcia </p><p>Año de creación: 2022 </p><p>Año actual:2022</p></footer>
+    <footer class="footer">
+        <div class="group-1">
+            <div class="box-1">
+                <div class="footer-img">
+                    <img class="img-logo" src="../Images/logo.PNG">
+                    <p class="GE">GE</p>
+                    <p class="xpenses">xpenses</p>
+                </div>
+            </div>
+            <div class="box-2">
+                <h2>SOBRE NOSOTROS</h2>
+                <p>Grupo formado por Joan Canals, Samuel García & Oscar Ramírez.</p>
+            </div>
+            <div class="box-3">
+                <p>Año creación: 2022-2023</p>
+            </div>
+        </div>
+    </footer>
 </body>
 </html>
--- Code/PHP/nav.php
+++ Code/PHP/nav.php
@@ -11,29 +11,25 @@
 
 <body>
     <div class="header">
+
+        <!--Imagen/Logo-->
         <div class="header-img">
             <img class="img-logo" src="../Images/logo.PNG">
+            <p class="GE">GE</p>
+            <p class="xpenses">xpenses</p>
         </div>
-        <div class="header-tittle">
-            <h1>GE</h1>
-            <h1 class="h1">XPENSES</h1>
-        </div>
+
+        <!--Menu del header-->
         <div class="header-nav">
             <nav class="navbar">
-
-
                 <ul class="navbar-bar">
-                    <li><a href="Home.php">Home</a></li>
-                    <li><a href="../GExpenses.php">Landing Page</a></li>
-                    <li><a href="cerrar_sesion.php">Cerrar Sesión</a></li>
-
+                    <a href="Home.php">Home</a>
+                    <a href="../GExpenses.php">Landing Page</a>
+                    <a href="cerrar_sesion.php">Cerrar Sesión</a>
                 </ul>
-
             </nav>
         </div>
     </div>
 
-
 </body>
-
 </html>
