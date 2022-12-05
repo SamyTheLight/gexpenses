ee4ca1e08150ef4f30d5d6b45c484e46f28530fd
SamyTheLight <samythelight@gmail.com>
2022-12-05 16:45:12 +0100

    index.php/css/JavaScript file

--- Code/index.php
+++ Code/index.php
@@ -113,7 +113,7 @@ if(isset($_GET['perRegistrar'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link rel="stylesheet" href="GExpenses.css">
+   <link rel="stylesheet" href="index.css">
     <link rel="stylesheet" href="PHP/footer.css">
     <title>Document</title>
 </head>
@@ -207,7 +207,3 @@ if(isset($_GET['perRegistrar'])){
 
 </html>
 
-<?php
-
-include 'PHP/footer.php';
-?>
