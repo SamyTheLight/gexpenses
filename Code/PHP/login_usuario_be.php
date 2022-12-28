fa559c7aa5be2c0107ef0beb5082423511fe28d3
JOANC\joanc <canals.ortiz.joan@alumnat.copernic.cat>
2022-12-28 16:03:30 +0100

    Canvi ruta imatges

--- Code/PHP/login_usuario_be.php
+++ Code/PHP/login_usuario_be.php
@@ -1,8 +1,8 @@
-<?php  
+<?php
 
 session_start();
 
-include 'ConexionDB.php'; 
+include 'ConexionDB.php';
 
 $usuarioLogin = $_POST['nombre'];
 $contrasenaUser = $_POST['contrasena'];
@@ -10,22 +10,22 @@ $userID = $_POST['id_usuario'];
 
 $validar_login = $conexion->prepare("SELECT * FROM usuario WHERE nombre = $usuarioLogin, 
 contrasena = $contrasenaUser and id_usuario = $userID");
-
+/*
 if(mysqli_num_rows($validar_login) > 0) {
    $_SESSION[`usuario`] = $usuarioLogin;
    header("location: Home.php");
    exit;
+}*/
+
+$queryLogin->bindParam(":nombreUser", $usuarioLogin);
+$queryLogin->bindParam(":passwordUser", $contrasenaLogin);
+
+$queryLogin->execute();
+
+$user = $queryLogin->fetch(PDO::FETCH_ASSOC);
+
+
+if ($user) {
+   $_SESSION['usuario'] = $user['nombre'];
+   header("location: ../index.php");
 }
-
-$queryLogin -> bindParam(":nombreUser",$usuarioLogin);
-$queryLogin -> bindParam(":passwordUser",$contrasenaLogin);
-
-$queryLogin-> execute();
-
-$user= $queryLogin->fetch(PDO::FETCH_ASSOC);
-
-
- if($user){
-    $_SESSION['usuario'] = $user['nombre'];
-    header("location: ../index.php");
- }
