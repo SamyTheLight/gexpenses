c46360170fff36e039a6b0ff8436439aad2525df
JOANC\joanc <canals.ortiz.joan@alumnat.copernic.cat>
2023-01-09 19:38:33 +0100

    Nav username done

--- Code/PHP/LandingPage.php
+++ Code/PHP/LandingPage.php
@@ -58,6 +58,7 @@ if ((isset($_POST['buttonLogin']))) {
 
     $nameuserL = $_POST['usernameLogin'];
 
+
     $passwordL = $_POST['passwordLogin'];
 
 
@@ -65,16 +66,17 @@ if ((isset($_POST['buttonLogin']))) {
 
     $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $queryLogin = $conexion->prepare("SELECT contrasena FROM usuario WHERE nombre = :nombreUser");
+    $queryLogin = $conexion->prepare("SELECT nombre,contrasena FROM usuario WHERE nombre = :nombreUser");
 
     $queryLogin->bindParam(':nombreUser', $nameuserL);
+
 
     $queryLogin->execute();
 
     $user = $queryLogin->fetch(PDO::FETCH_ASSOC);
 
     if (password_verify($passwordL, $user['contrasena'])) {
-        $_SESSION['usuario'] = $user['nombre'];
+        $_SESSION['nombre'] = $nameuserL;
         header("location: PHP/Home.php");
     }
 }
