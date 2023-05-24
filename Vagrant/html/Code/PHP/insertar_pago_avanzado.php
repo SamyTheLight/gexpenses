5599a5d61c85dcb631d0a46cc5a82bda2211bbbd
Samuel García Dominguez <samythelight@gmail.com>
2023-05-24 22:47:53 +0200

    pago avanzado planteamiento
    Añadido pagoAvanzado.js & insertar_pago_avanzado.php

---
+++ Vagrant/html/Code/PHP/insertar_pago_avanzado.php
@@ -0,0 +1,11 @@
+<?php
+
+//TODO recibir el post id_gasto, cantidad total, pagador, id_deudores[], deuda[]
+// hacer un sumatorio de la deuda
+// si el sumatorio es igual a la cantidad total
+// si el sumatorio == cantidad del gasto en BD
+// insertas el pago_avanzado en BD
+
+
+
+?>
---
+++ Vagrant/html/Code/Scripts/pagoAvanzado.js
@@ -0,0 +1,19 @@
+function sumatorio(deudaDeCadaUsuario){
+    let sumatorio = 0;
+    //Sumarlo todo en el sumatorio
+    return sumatorio;
+}
+
+function comprobarCantidad(totalGasto, sumatorioDeuda){
+    return totalGasto -sumatorioDeuda;
+}
+
+//Si comprobarCantidad == 0, ya se puede mandar el formulario
+
+//Funcion en javascript que te valide el formulario, obteniendo de los INPUTS las cantidades del sumatorio y pasarselas a la función sumatorio
+
+//eventos en los inputs para validar que metes un número entero o un decimal de 2 posiciones
+//calcular el sumatorio de todos los inputs a la vez
+
+//en el php anteriomente 1 consulta para el los datos del pago, 1 consulta para obtener el array de los los usuarios del pago
+//for o un foreach -> input hidden name=id_usuario[], label nombre del usuario ,input name=deuda[]
